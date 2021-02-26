bf4530280a5ced721c8a7ab42a76e232a4427c39
ksyuvasilek <ks8715@mail.ru>
2021-02-26 21:31:17 +0700

    Add simple_real to add new function

---
+++ simple_real.cpp
@@ -0,0 +1,9 @@
+#include "simple_atd.h"
+using namespace std;
+namespace simple_numbers {
+	// Приведение комплексного к действительному
+	int simple::Real()
+	{
+		return a/b;
+	}
+} // end simple_numbers namespace
