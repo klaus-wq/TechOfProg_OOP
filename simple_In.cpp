8f93fe9db683e5fa89e913c47d35f0c23fb0324e
ksyuvasilek <ks8715@mail.ru>
2021-02-26 21:48:27 +0700

    Update simple_In to add new field

--- simple_In.cpp
+++ simple_In.cpp
@@ -4,5 +4,6 @@ namespace simple_numbers {
 	// Ввод параметров простой дроби
 	void simple::InData(ifstream& ifst) {
 		ifst >> a >> b;
+		number::InData(ifst);
 	}
 } // end simple_numbers namespace
