300d81082e70d10dbd8202d5250b57fc8234bd1e
ksyuvasilek <ks8715@mail.ru>
2021-02-26 21:48:47 +0700

    Update simple_Out to add new field

--- simple_Out.cpp
+++ simple_Out.cpp
@@ -4,5 +4,7 @@ namespace simple_numbers {
 	// Вывод параметров простой дроби
 		void simple::Out(ofstream & ofst) {
 		ofst << "It is simple fraction: a = " << a << ", b = " << b << endl;
+		number::Out(ofst);
+		ofst << endl;
 	}
 } // end simple_numbers namespace
