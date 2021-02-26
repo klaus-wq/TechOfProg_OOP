fd8a8210ddf7bb769c05f7df7b6746cdeaebd6e5
ksyuvasilek <ks8715@mail.ru>
2021-02-26 21:48:04 +0700

    Update complex_Out to add new field

--- complex_Out.cpp
+++ complex_Out.cpp
@@ -4,5 +4,7 @@ namespace simple_numbers {
 	// Вывод параметров комплексного числа
 	void complex::Out(ofstream& ofst) {
 			ofst << "It is complex number: x = " << x << ", y = " << y << endl;
+			number::Out(ofst);
+			ofst << endl;
 	}
 } // end simple_numbers namespace
