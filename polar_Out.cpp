1fc412111dc51ea09a970605cb247e69aacb11b8
ksyuvasilek <ks8715@mail.ru>
2021-02-26 21:06:35 +0700

    Add polar_Out to output a new class

---
+++ polar_Out.cpp
@@ -0,0 +1,8 @@
+#include "polar_atd.h"
+using namespace std;
+namespace simple_numbers {
+	// Вывод полярных координат
+	void polar::Out(ofstream& ofst) {
+		ofst << "It is polar: r = " << r << ", fi = " << fi << endl;
+	}
+} // end simple_numbers namespace
